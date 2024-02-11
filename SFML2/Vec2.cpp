dd1d9294969dd398974f726af78e23f34dec73b3
cofi420 <filip.vidakovic.04@gmail.com>
2024-02-11 21:18:09 +0100

    Implement Vec2 class

--- SFML2/Vec2.cpp
+++ SFML2/Vec2.cpp
@@ -1,2 +1,83 @@
 
 #include "Vec2.h"
+
+Vec2::Vec2() :x(0), y(0)
+{
+
+}
+
+Vec2::Vec2(float xin, float yin) : x(xin), y(yin)
+{
+
+}
+
+bool Vec2::operator==(const Vec2& rhs) const
+{
+	return x == rhs.x && y == rhs.y;
+}
+
+bool Vec2::operator!=(const Vec2& rhs) const
+{
+	return x != rhs.x || y != rhs.y;
+}
+
+Vec2 Vec2::operator+(const Vec2& rhs) const
+{
+	return Vec2(x + rhs.x, y + rhs.y);
+}
+
+Vec2 Vec2::operator-(const Vec2& rhs) const
+{
+	return Vec2(x - rhs.x, y - rhs.y);
+}
+
+Vec2 Vec2::operator*(const float val) const
+{
+	return Vec2(x * val, y * val);
+}
+
+Vec2 Vec2::operator/(const float val) const
+{
+	return Vec2(x / val, y / val);
+}
+
+void Vec2::operator+=(const Vec2& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+}
+
+void Vec2::operator-=(const Vec2& rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+}
+
+void Vec2::operator*=(const float val)
+{
+	x *= val;
+	y *= val;
+}
+
+void Vec2::operator/=(const float val)
+{
+	x /= val;
+	y /= val;
+}
+
+float Vec2::dist(const Vec2& rhs) const
+{
+	int dx = rhs.x - x;
+	int dy = rhs.y - y;
+	return std::sqrt((dx * dx) + (dy * dy));
+}
+
+float Vec2::length() const
+{
+	return std::sqrt((x * x) + (y * y));
+}
+
+Vec2 Vec2::norm() const
+{
+	return Vec2(x / this->length(), y / this->length());
+}
