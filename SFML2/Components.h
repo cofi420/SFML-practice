5ad29cae20ac813d3bbff49498f1c7e8b5652380
Bringsla <ivradov@gmail.com>
2024-02-11 20:14:41 +0100

    Added SFML support and Components.h

--- SFML2/Components.h
+++ SFML2/Components.h
@@ -1,5 +1,62 @@
 #pragma once
-class Components
+
+#include "Vec2.h"
+#include <SFML/Graphics.hpp>
+
+class CTransform
 {
+public:
+	Vec2 pos = { 0.0 , 0.0 };
+	Vec2 velocity = { 0.0 , 0.0 };
+	float angle;
+
+	CTransform(const Vec2& p, const Vec2& b, float u): pos(p), velocity(b), angle(u) {}
 };
+
+class CShape
+{
+public:
+	sf::CircleShape circle;
+	CShape(float r,int points, const sf::Color & fill, const sf::Color & outline, float thickness): circle(r,points)
+	{
+		circle.setFillColor(fill);
+		circle.setOutlineColor(outline);
+		circle.setOutlineThickness(thickness);
+		circle.setOrigin(r, r);
+	}
+};
+
+class CCollision
+{
+public:
+	float radius = 0;
+	CCollision(float r):radius(r){}
+};
+
+class CScore
+{
+public:
+	int score;
+	CScore(int s) : score(s){}
+};
+
+class CLifespan
+{
+public:
+	int remaining = 0;
+	int total = 0;
+	CLifespan(int a): remaining(a), total(a){}
+};
+
+class CInput
+{
+public:
+	bool up;
+	bool down;
+	bool left;
+	bool right;
+
+	CInput(){}
+};
+
 
